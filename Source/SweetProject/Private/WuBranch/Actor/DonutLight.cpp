48780938bde867b3ed10ccfb6fd61e44ef763ef1
gene <cg100111@gmail.com>
2025-07-12 02:30:35 +0900

    ドーナツの形のライトのクラスを作った

---
+++ Source/SweetProject/Private/WuBranch/Actor/DonutLight.cpp
@@ -0,0 +1,63 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WuBranch/Actor/DonutLight.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+#include "Components/SpotLightComponent.h"
+#include "WuBranch/Actor/Weapon.h"
+
+// Sets default values
+ADonutLight::ADonutLight()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	//PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+
+	DonutMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DonutMesh"));
+	DonutMesh->SetupAttachment(RootComponent);
+
+	LightMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LightMesh"));
+	LightMesh->SetupAttachment(RootComponent);
+
+	Collision = CreateDefaultSubobject<UBoxComponent>(TEXT("Collision"));	
+	Collision->SetupAttachment(RootComponent);
+	
+	Light = CreateDefaultSubobject<USpotLightComponent>(TEXT("Light"));
+	Light->SetupAttachment(RootComponent);
+
+}
+
+// Called when the game starts or when spawned
+void ADonutLight::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	Collision->OnComponentBeginOverlap.AddDynamic(this, &ADonutLight::OnFireOverlapBegin);
+}
+
+// Called every frame
+//void ADonutLight::Tick(float DeltaTime)
+//{
+//	Super::Tick(DeltaTime);
+//
+//}
+
+void ADonutLight::OnFireOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor)
+	{
+		// 燃やせるものが近づいたら、ライトを点灯させる
+		if (OtherActor->IsA(AWeapon::StaticClass()))
+		{
+			Light->SetVisibility(true);
+			UE_LOG(LogTemp, Warning, TEXT("Donut Light Activated!"));
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Non-flammable object detected."));
+		}
+	}
+}
+
---
+++ Source/SweetProject/Public/WuBranch/Actor/DonutLight.h
@@ -0,0 +1,61 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DonutLight.generated.h"
+
+class UBoxComponent;
+class USpotLightComponent;
+
+UCLASS()
+class SWEETPROJECT_API ADonutLight : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADonutLight();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	//virtual void Tick(float DeltaTime) override;
+
+private:
+
+	/// <summary>
+	/// 燃やせるものが近つく時
+	/// </summary>
+	UFUNCTION()
+	void OnFireOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	/// <summary>
+	/// ドーナツのメッシュ
+	/// </summary>
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* DonutMesh;
+
+	/// <summary>
+	/// ライトのメッシュ
+	/// </summary>
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* LightMesh;
+
+	/// <summary>
+	/// ドーナツのコリジョン
+	/// </summary>
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* Collision;
+
+	/// <summary>
+	/// ドーナツのライト
+	/// </summary>
+	UPROPERTY(EditAnywhere)
+	USpotLightComponent* Light;
+
+};
