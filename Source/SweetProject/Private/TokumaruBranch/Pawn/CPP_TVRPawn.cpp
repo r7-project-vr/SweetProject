dc192a967b5bb384850ed7040eb420a19e246a34
tokumaru3325 <furufuru3325@gmail.com>
2025-07-16 09:37:30 +0900

    しゃがみ

---
+++ Source/SweetProject/Private/TokumaruBranch/Pawn/CPP_TVRPawn.cpp
@@ -0,0 +1,73 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TokumaruBranch/Pawn/CPP_TVRPawn.h"
+
+// Sets default values
+ACPP_TVRPawn::ACPP_TVRPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
+	if (!CapsuleComp) {
+		UE_LOG(LogTemp, Warning, TEXT("CapsuleCompNull"));
+		return;
+	}
+	else {
+		RootComponent = CapsuleComp;
+	}
+
+	VROrigin = CreateDefaultSubobject<USceneComponent>(TEXT("VROrigin"));
+	if (!VROrigin) {
+		UE_LOG(LogTemp, Warning, TEXT("VROriginNull"));
+		return;
+	}
+	else {
+		VROrigin->SetupAttachment(RootComponent);
+	}
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	if (!Camera) {
+		UE_LOG(LogTemp, Warning, TEXT("CameraNull"));
+		return;
+	}
+	else {
+		Camera->SetupAttachment(VROrigin);
+	}
+
+
+}
+
+// Called when the game starts or when spawned
+void ACPP_TVRPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	InitialCameraZ = Camera->GetRelativeLocation().Z;
+}
+
+// Called every frame
+void ACPP_TVRPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	float CurrentZ = Camera->GetRelativeLocation().Z;
+	float DeltaZ = InitialCameraZ - CurrentZ;
+
+	if (DeltaZ > 30.0f) // 30cm
+	{
+		// カプセルの高さ変更
+		CapsuleComp->SetCapsuleHalfHeight(44.0f); // しゃがみ用サイズ
+	}
+	else
+	{
+		CapsuleComp->SetCapsuleHalfHeight(88.0f); // 通常サイズ
+	}
+}
+
+// Called to bind functionality to input
+void ACPP_TVRPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/SweetProject/Public/TokumaruBranch/Pawn/CPP_TVRPawn.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include <Components/CapsuleComponent.h>
+#include <Camera/CameraComponent.h>
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "CPP_TVRPawn.generated.h"
+
+UCLASS()
+class SWEETPROJECT_API ACPP_TVRPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ACPP_TVRPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UCapsuleComponent* CapsuleComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	USceneComponent* VROrigin;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UCameraComponent* Camera;
+
+	float InitialCameraZ = 0.0f;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
