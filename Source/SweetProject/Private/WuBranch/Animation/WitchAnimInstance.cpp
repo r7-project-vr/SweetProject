111f74e6747186c502251e60e522c4fc58667117
ウー <cg100111@gmail.com>
2025-09-01 13:45:00 +0900

    魔女のアニメーションをアニメーションブループリントで再生する
    攻撃終了の通知を作った

---
+++ Source/SweetProject/Private/WuBranch/Animation/WitchAnimInstance.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WuBranch/Animation/WitchAnimInstance.h"
+#include "MurasameBranch/WitchBossActor.h"
+
+UWitchAnimInstance::UWitchAnimInstance()
+	: Witch(nullptr)
+	, IsAttack(false)
+{
+}
+
+void UWitchAnimInstance::UpdateAnimation(float DeltaTime)
+{
+	if (!Witch)
+		SetWitchInstance();
+
+	if(Witch)
+		IsAttack = Witch->GetIsAttack();
+}
+
+void UWitchAnimInstance::SetWitchInstance()
+{
+	if (AWitchBossActor* Actor = Cast<AWitchBossActor>(TryGetPawnOwner()))
+	{
+		Witch = Actor;
+	}
+}
