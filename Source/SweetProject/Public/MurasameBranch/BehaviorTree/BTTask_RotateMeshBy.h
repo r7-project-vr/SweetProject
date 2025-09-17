5dab3c4c0a2a962fa515d6b681a665d9a67f0b65
0404WangYunze <wangyunze449129954@gmail.com>
2025-09-17 12:27:21 +0900

    WaffleのSkeletalMeshを回転処理BTTask追加した

---
+++ Source/SweetProject/Public/MurasameBranch/BehaviorTree/BTTask_RotateMeshBy.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_RotateMeshBy.generated.h"
+
+/**
+ * AIのスケルトンメッシュを回転処理する、アクター本体をそのまま
+ * 
+ */
+UCLASS()
+class SWEETPROJECT_API UBTTask_RotateMeshBy : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UBTTask_RotateMeshBy();
+
+	UPROPERTY(EditAnywhere, Category = "Rotation")
+	float RotationYaw;
+
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+};
